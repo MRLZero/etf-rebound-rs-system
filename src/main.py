704d293d5a31f508b8f9378f6374cced28d0e216
MRLZero <756270912@qq.com>
2026-05-23 17:08:25 +0800

    commit

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@
 from config import ETFS
 from market import get_market_regime
 from etf_analyzer import analyze
-from notifier import send_tg
+from notifier import send_tg, send_telegram_messages
+
 
 def main():
+    results = []
     market = get_market_regime()
 
     msg = "📈 *ETF RS Rebound Signals*\n\n"
@@ -13,38 +15,20 @@ def main():
         f"VOO > MA200: {'✅' if market['voo_bull'] else '❌'}\n"
         f"QQQ > MA200: {'✅' if market['qqq_bull'] else '❌'}\n\n"
     )
-
-    found = False
+    send_tg(msg)
 
     for symbol, item in ETFS.items():
         try:
             window, category = item.get("window", 180), item.get("category", "Unknown")
             result = analyze(symbol, window, market)
             if result:
-                found = True
-                # category = symbol in ETFS and ETFS[symbol] or "Unknown"
-                msg += (
-                    f"{result['symbol']} {category}\n"
-                    f"Price: ${result['price']}\n"
-                    f"Recent High: ${result['high']}\n"
-                    f"Recent Low: ${result['low']}\n"
-                    f"cDrawdown: {result['drawdown']}%\n"
-                    f"cRebound: +{result['rebound']}%\n"
-                    f"Recovery Ratio: {result['recovery_ratio']}\n"
-                    f"pDrawdown: {result['pre_drawdown']}%\n"
-                    # f"RS Ratio: {result['rs']}\n"
-                    f"Window: {result['window']}\n"
-                    f"{result['state']} \n\n"
-                )
+                result["category"] = category
+                results.append(result)
+
         except Exception as e:
             print(f"{symbol} error: {e}")
 
-    if not found:
-        msg += "No strong RS rebound setup today."
-
-    print(msg)
-    send_tg(msg)
-
+    send_telegram_messages(results)
 
 if __name__ == "__main__":
     main()
--- src/notifier.py
+++ src/notifier.py
@@ -5,6 +5,7 @@ import requests
 TG_TOKEN = os.getenv("TG_TOKEN")
 TG_CHAT_ID = os.getenv("TG_CHAT_ID")
 
+
 def send_tg(msg):
     """发送Telegram消息"""
     url = f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage"
@@ -16,6 +17,136 @@ def send_tg(msg):
             "parse_mode": "Markdown"
         }
     )
+
+
+from math import ceil
+
+
+def build_messages(results, max_items=15):
+
+    # -----------------------------
+    # 过滤 No-Trade
+    # -----------------------------
+    # filtered = [
+    #
+    #     r for r in results
+    #
+    #     if r["state"] != "⚪ No-Trade"
+    # ]
+    filtered = [
+        r for r in results
+        if r is not None
+    ]
+
+    # -----------------------------
+    # 信号优先级
+    # -----------------------------
+    priority = {
+
+        "🚀 STRONG BUY": 1,
+        "🚀 BUY": 2,
+        "⚪ WATCH": 3,
+        "⚪ No-Trade": 4
+    }
+
+    # -----------------------------
+    # 排序
+    # 1. signal
+    # 2. recovery_ratio
+    # -----------------------------
+    filtered.sort(
+
+        key=lambda x: (
+
+            priority.get(x["state"], 99),
+
+            x["recovery_ratio"]
+        )
+    )
+
+    # -----------------------------
+    # 拆分消息
+    # -----------------------------
+    total_msgs = ceil(
+        len(filtered) / max_items
+    )
+
+    messages = []
+
+    for i in range(total_msgs):
+
+        chunk = filtered[
+            i * max_items:
+            (i + 1) * max_items
+        ]
+
+        msg = (
+            f"📊 ETF/Stock Quant Signals "
+            f"({i+1}/{total_msgs})\n\n"
+        )
+
+        current_signal = None
+
+        for result in chunk:
+
+            # -----------------------------
+            # 新 signal section
+            # -----------------------------
+            if result["state"] != current_signal:
+
+                current_signal = result["state"]
+
+                msg += f"\n{current_signal}\n\n"
+
+            # -----------------------------
+            # 行内容
+            # -----------------------------
+            line = (
+
+                f"{result['symbol']} {result['category']}\n"
+                f"Price: ${result['price']}\n"
+                f"Recent High: ${result['high']}\n"
+                f"Recent Low: ${result['low']}\n"
+                f"cDrawdown: {result['drawdown']}%\n"
+                f"cRebound: +{result['rebound']}%\n"
+                f"Recovery Ratio: {result['recovery_ratio']}\n"
+                f"pDrawdown: {result['pre_drawdown']}%\n"
+                # f"RS Ratio: {result['rs']}\n"
+                f"Window: {result['window']}\n\n"
+                # f"{result['state']} \n\n"
+
+            )
+
+            msg += line
+
+        messages.append(msg)
+
+    return messages
+
+
+# -----------------------------
+# Send Telegram Messages
+# -----------------------------
+def send_telegram_messages(results):
+
+    """
+    Telegram 推送
+    """
+
+    try:
+
+        messages = build_messages(results)
+        for msg in messages:
+            send_tg(msg)
+            print(msg)
+
+        print(
+            f"Telegram sent: "
+            f"{len(messages)} messages"
+        )
+
+    except Exception:
+        print("Telegram send error")
 
 if __name__ == '__main__':
     msg = "Hello!"
